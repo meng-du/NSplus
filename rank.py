cf9df3c86eb684a46aacbecbe0f7f686a3578485
Meng Du <mengdu@umich.edu>
2017-06-06 11:36:20 -0700

    new rank files; changingg predictor

--- rank.py
+++ rank.py
@@ -2,35 +2,34 @@ from metaext import *
 import os
 
 if __name__ == '__main__':
-    MASK_FOLDER = 'mPFC_masks_20170207'
+    MASK_FOLDER = 'dACC_parcellations_170605/'
     maskFiles = [mask for mask in os.listdir(MASK_FOLDER) if mask[0] != '.']
-    log = []
     for maskFile in maskFiles:
         dataset = ns.Dataset(filename='current_data/database.txt', masker=MASK_FOLDER + '/' + maskFile)
         dataset.add_features('current_data/features.txt')
         print 'dataset loaded'
         print maskFile
 
-        maskdir = 'results_v5/pairwise_conjunctions_1/' + maskFile[:-4] + '/'
-        maskcsvs = [csvfile for csvfile in os.listdir(maskdir) if csvfile.endswith('csv')]
-        for maskcsv in maskcsvs:
-            with open(maskdir + maskcsv, 'r') as csvfile:
-                content = list(csv.reader(csvfile))[2:]
-                mask = np.array([int(row[0][0]) for row in content]) == 4
-            print maskcsv
-            term = maskcsv[:maskcsv.index('_pFgA_given_pF')]
-            print term, mask.sum()
-            log.append((term, mask.sum()))
+        rank_avg_rank(dataset,
+                      rank_by='pFgA_given_pF=0.50',
+                      csv_file=maskFile[:-4] + '_rank.csv')
 
-            rank_avg_voxel(dataset,
-                           rank_by='pFgA_given_pF=0.50',
-                           extra_expr=[
-                              '(social | mentalizing)',
-                              'emotion*',
-                              '(value | reward | incentive)',
-                              '(episodic | future | past | retrieval | prospective | memory retrieval)'],
-                           csv_file=term + '_' + maskFile[:-4] + '_rank.csv',
-                           mask=mask)
-    import json
-    with open('log.json', 'w') as outfile:
-        outfile.write(json.dumps(log))
+        # maskdir = 'results_v5/pairwise_conjunctions_1/' + maskFile[:-4] + '/'
+        # maskcsvs = [csvfile for csvfile in os.listdir(maskdir) if csvfile.endswith('csv')]
+        # for maskcsv in maskcsvs:
+        #     with open(maskdir + maskcsv, 'r') as csvfile:
+        #         content = list(csv.reader(csvfile))[2:]
+        #         mask = np.array([int(row[0][0]) for row in content]) == 4
+        #     print maskcsv
+        #     term = maskcsv[:maskcsv.index('_pFgA_given_pF')]
+        #     print term, mask.sum()
+        #
+        #     rank_avg_voxel(dataset,
+        #                    rank_by='pFgA_given_pF=0.50',
+        #                    extra_expr=[
+        #                       '(social | mentalizing)',
+        #                       'emotion*',
+        #                       '(value | reward | incentive)',
+        #                       '(episodic | future | past | retrieval | prospective | memory retrieval)'],
+        #                    csv_file=term + '_' + maskFile[:-4] + '_rank.csv',
+        #                    mask=mask)
--- term_predictor.py
+++ term_predictor.py
@@ -2,8 +2,10 @@ import neurosynth as ns
 import numpy as np
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVR, LinearSVR
+from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor
 
-dataset = ns.Dataset.load('current_data/dataset.pkl')
+
+dataset = ns.Dataset.load('dataset.pkl')
 features = dataset.get_feature_data()  # get term frequencies, shape = (11406, 3169)
 activations = dataset.get_image_data().T  # get activations, shape = (11406, 228453)
 assert np.all(features.index == dataset.image_table.ids)  # make sure the ids match
@@ -14,22 +16,57 @@ def tag_accuracy(estimator, X, y):
     # TODO
     return 1.0
 
-# rbf SVR
+# regression
+# linear SVR
+# lsvr = GridSearchCV(
+#     estimator=LinearSVR(),
+#     param_grid={'C': 10.0 ** np.arange(-3, 3)},
+#     scoring='r2',
+#     cv=10,
+#     n_jobs=-1  # use all CPUs
+# )
+# lsvr.fit(features, activations)  # X: shape = [n_samples, n_features], y: shape = [n_samples] or [n_samples, n_output]
+# print lsvr.cv_results_
+# print 'BEST SCORE', lsvr.best_score_
+# print 'BEST PARAMS', lsvr.best_params_
+# # rbf SVR
 # svr = GridSearchCV(
 #     estimator=SVR(),
 #     param_grid={'gamma': 10.0 ** np.arange(-5, 2), 'C': 10.0 ** np.arange(-3, 3)},
 #     scoring='r2',
 #     cv=10,
-#     n_jobs=-1  # use all CPUs
+#     n_jobs=-1
 # )
-# linear SVR
-lsvr = GridSearchCV(
-    estimator=LinearSVR(),
-    param_grid={'C': 10.0 ** np.arange(-3, 3), 'dual': [False], 'epsilon': [0], 'verbose': [10]},
+# svr.fit(features, activations)
+# print svr.cv_results_
+# print 'BEST SCORE', svr.best_score_
+# print 'BEST PARAMS', svr.best_params_
+# random forest
+# TODO CV unnecessary?
+rf = GridSearchCV(
+    estimator=RandomForestRegressor(),
+    param_grid={'n_estimators': [20, 40, 80, 160, 800, 1600], 'max_features': [0.33, 'sqrt'],
+                'min_samples_split': [3, 5, 7]},
     scoring='r2',
     cv=10,
     n_jobs=-1
 )
-lsvr.fit(features, activations)  # X: shape = [n_samples, n_features], y: shape = [n_samples] or [n_samples, n_output]
-
-# Random Forest
+rf.fit(features, activations)
+print rf.cv_results_
+print 'BEST SCORE', rf.best_score_
+print 'BEST PARAMS', rf.best_params_
+# TODO refit and see oob score
+# Extremely Randomized Trees
+et = GridSearchCV(
+    estimator=ExtraTreesRegressor(),
+    param_grid={'n_estimators': [20, 40, 80, 160, 800, 1600], 'max_features': [0.33],
+                'min_samples_split': [3, 5, 7], 'oob_score': [True]},
+    scoring='r2',
+    cv=10,
+    n_jobs=-1
+)
+et.fit(features, activations)
+print et.cv_results_
+print 'BEST SCORE', et.best_score_
+print 'BEST PARAMS', et.best_params_
+# TODO refit and see oob score
