3ef16661e44a6d4d4d4fa21ff183c2aa48d55427
Meng Du <mengdu@umich.edu>
2017-08-25 16:34:25 -0700

    nothing is working

--- term_predictor.py
+++ term_predictor.py
@@ -11,11 +11,15 @@ os.environ["JOBLIB_TEMP_FOLDER"] = '/u/scratch2/m/mengdu/'
 # logging
 logging.basicConfig(filename='results.log', level=logging.INFO, format='%(asctime)s %(message)s')
 
-dataset = ns.Dataset.load('dataset.pkl')
+# dataset = ns.Dataset(filename='current_data/database.txt')#, masker='BA10_d1_mid_ventral.nii')
+# dataset.add_features('current_data/features.txt')
+dataset.save('current_data/dataset.pkl')
+quit()
+dataset = ns.Dataset.load('current_data/dataset.pkl')  #ba10mv_
 features = dataset.get_feature_data()  # get term frequencies, shape = (11406, 3169)
 activations = dataset.get_image_data().T  # get activations, shape = (11406, 228453)
 assert np.all(features.index == dataset.image_table.ids)  # make sure the ids match
-logging.info('Neurosynth loaded')
+logging.info('Neurosynth database loaded')
 
 
 # customized scoring function
@@ -54,40 +58,40 @@ print svr.cv_results_
 print '-------------\n'
 logging.info(['BEST SVR SCORE', svr.best_score_])
 logging.info(['BEST SVR PARAMS', svr.best_params_])
-# random forest
-# TODO CV unnecessary for rf?
-rf = GridSearchCV(
-    estimator=RandomForestRegressor(),
-    param_grid={'n_estimators': [20, 40, 80, 160, 800, 1600], 'max_features': [0.33, 'sqrt'],
-                'min_samples_split': [3, 5, 7]},
-    scoring='r2',
-    cv=10,
-    n_jobs=-1,
-    verbose=2
-)
-rf.fit(features, activations)
-print '\n-------------'
-print rf.cv_results_
-print '-------------\n'
-logging.info(['BEST RF SCORE', rf.best_score_])
-logging.info(['BEST RF PARAMS', rf.best_params_])
-# TODO refit and see oob score
-# Extremely Randomized Trees
-et = GridSearchCV(
-    estimator=ExtraTreesRegressor(),
-    param_grid={'n_estimators': [20, 40, 80, 160, 800, 1600], 'max_features': [0.33],
-                'min_samples_split': [3, 5, 7], 'oob_score': [True]},
-    scoring='r2',
-    cv=10,
-    n_jobs=-1,
-    verbose=2
-)
-et.fit(features, activations)
-print '\n-------------'
-print et.cv_results_
-print '-------------\n'
-logging.info(['BEST ET SCORE', et.best_score_])
-logging.info(['BEST ET PARAMS', et.best_params_])
+# # random forest
+# # TODO CV unnecessary for rf?
+# rf = GridSearchCV(
+#     estimator=RandomForestRegressor(),
+#     param_grid={'n_estimators': [20, 40, 80, 160, 800, 1600], 'max_features': [0.33, 'sqrt'],
+#                 'min_samples_split': [3, 5, 7]},
+#     scoring='r2',
+#     cv=10,
+#     n_jobs=-1,
+#     verbose=2
+# )
+# rf.fit(features, activations)
+# print '\n-------------'
+# print rf.cv_results_
+# print '-------------\n'
+# logging.info(['BEST RF SCORE', rf.best_score_])
+# logging.info(['BEST RF PARAMS', rf.best_params_])
+# # TODO refit and see oob score
+# # Extremely Randomized Trees
+# et = GridSearchCV(
+#     estimator=ExtraTreesRegressor(),
+#     param_grid={'n_estimators': [20, 40, 80, 160, 800, 1600], 'max_features': [0.33],
+#                 'min_samples_split': [3, 5, 7], 'oob_score': [True]},
+#     scoring='r2',
+#     cv=10,
+#     n_jobs=-1,
+#     verbose=2
+# )
+# et.fit(features, activations)
+# print '\n-------------'
+# print et.cv_results_
+# print '-------------\n'
+# logging.info(['BEST ET SCORE', et.best_score_])
+# logging.info(['BEST ET PARAMS', et.best_params_])
 # TODO refit and see oob score
 # importances = forest.feature_importances_
 # std = np.std([tree.feature_importances_ for tree in forest.estimators_],
