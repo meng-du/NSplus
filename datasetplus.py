8bc924b84c0729676a85bdf886fc29fa7f38a3e1
Meng Du <mengdu@umich.edu>
2018-05-25 12:13:07 -0700

    minor

--- datasetplus.py
+++ datasetplus.py
@@ -4,13 +4,17 @@ import neurosynth as ns
 
 class DatasetPlus(ns.dataset):
     """
-    Neurosynth Dataset with a refactored constructor
+    The Neurosynth Dataset class with a refactored constructor
     """
     def __init__(self, filename, feature_filename=None, masker=None, r=6,
             transform=True, target='MNI', **kwargs):
         # ns.Dataset.__init__(*args, **kwargs)
         # Instance properties
         self.r = r
+        self.masker = masker
+        self.default_masker = ns.mask.Masker(
+            os.path.join(os.path.dirname(__file__), os.path.pardir,
+                         'resources', 'MNI152_T1_2mm_brain.nii.gz'))  # TODO
 
         # Set up transformations between different image spaces
         if transform:
@@ -25,21 +29,14 @@ class DatasetPlus(ns.dataset):
         # Load and process activation data
         self.activations = self._load_activations(filename)
 
-        # Load the volume into a new Masker
-        if masker is None:
-            resource_dir = os.path.join(os.path.dirname(__file__),
-                                        os.path.pardir,
-                                        'resources')
-            masker = os.path.join(
-                resource_dir, 'MNI152_T1_2mm_brain.nii.gz')
-        self.masker = ns.mask.Masker(masker)
-
         # Create supporting tables for images and features
         self.create_image_table()
         if feature_filename is not None:
             self.add_features(feature_filename, **kwargs)
 
     def reset_mask(self):
+        if self.masker is None:
+            return
         self.masker.unmask() # ???
 
     def add_mask(self, mask_file):
